1967bfa24d39437cc1aa23868051c6633e3fba78
somayeh_majidi <somayeh_majidi@outlook.com>
2025-11-24 22:35:07 +0330

    add and update about me

---
+++ src/app/about/about.ts
@@ -0,0 +1,14 @@
+import { Component, AfterViewInit, OnDestroy, ViewChild, ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  imports: [],
+  templateUrl: './about.html',
+  styleUrl: './about.css',
+})
+export class About {
+
+  download() {
+    
+  }
+}
