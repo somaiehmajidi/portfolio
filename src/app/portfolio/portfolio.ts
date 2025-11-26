719adf074d8177cf0dc73bbf771a7d4bd8755c83
somayeh_majidi <somayeh_majidi@outlook.com>
2025-11-26 12:44:21 +0330

    update latest projects

--- src/app/portfolio/portfolio.ts
+++ src/app/portfolio/portfolio.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, signal, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { data } from './projects.data'
 
 @Component({
   selector: 'app-portfolio',
   imports: [],
   templateUrl: './portfolio.html',
   styleUrl: './portfolio.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class Portfolio {
+export class Portfolio implements OnInit{
+  items = signal([...data])
 
+  ngOnInit(): void {
+    
+  }
 }
---
+++ src/app/portfolio/projects.data.ts
@@ -0,0 +1,58 @@
+export const data = [
+    {
+        id: 1,
+        title: 'ISOS telehealth',
+        company: 'SOS Iran',
+        link: 'https://isos.clinic/',
+        img: 'img/projects/isos.jpg',
+        tag: 'angular',
+        tech: [
+            'Angular',
+            'Angular Material',
+            'WebRTC/SignalR',
+            'GraphQL/Apollo'
+        ] 
+    },
+    {
+        id: 2,
+        title: 'Mores Business panel',
+        company: 'Segal Mehr',
+        link: 'https://mores.ir/en/',
+        img: 'img/projects/isos.jpg',
+        tag: 'vue',
+        tech: [
+            'Vue 2 & 3',
+            'Vuetify',
+            'Jest/Vitest',
+            'HighChart'
+        ] 
+    },
+    {
+        id: 3,
+        title: 'Hamgardi Travel Magazine',
+        company: 'Mehr Hamgardi',
+        link: 'https://hamgardi.com/',
+        img: 'img/projects/isos.jpg',
+        tag: 'js',
+        tech: [
+            'HTML5',
+            'CSS3',
+            'jQuery',
+            'JavaScript'
+        ] 
+    },
+    {
+        id: 4,
+        title: 'Makanchi B&B',
+        company: 'Mehr Hamgardi',
+        link: 'https://makanchi.com/',
+        img: 'img/projects/isos.jpg',
+        tag: 'js',
+        tech: [
+            'HTML5',
+            'CSS3',
+            'jQuery',
+            'JavaScript'
+        ] 
+    }
+]
